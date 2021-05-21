aabaf721cd89ccf77836c35e3d5b7efe32ec9160
Diana <robarm@inbox.ru>
2021-05-21 11:02:22 +0300

    second commit

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by мас on 18.05.2021.
+//
+
+import UIKit
+ 
+class ProfileView : UIView {
+    @IBOutlet weak var picture : UIImageView!
+    @IBOutlet weak var text : UITextView!
+    @IBOutlet weak var date : UILabel!
+    @IBOutlet weak var name : UILabel!
+    @IBOutlet weak var city : UILabel!
+}
